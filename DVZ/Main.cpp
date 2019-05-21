a96b3ebe59cc7326b25754fb1920b3587aa44832
MathDotSqrt <ctrenkov@gmail.com>
2019-05-21 09:57:27 -0400

    Created window with GLFW and GLEW. Fixed glew32.dll by placeing it in the debug and x64 folder

--- DVZ/Main.cpp
+++ DVZ/Main.cpp
@@ -1,20 +1,57 @@
-#include <entt.hpp>
-#include <gl/glew.h>
+#include <stdio.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-struct position {
-	float x, y, z;
-};
+#include <iostream>
 
-struct velocity {
-	float dx, dy, dz;
-};
+const static unsigned int DEFAULT_WIDTH = 1024, DEFAULT_HEIGHT = 768;
+const static char* TITLE = "Dwavres Vs Zombies";
 
-int main() {
+GLFWwindow* initGLFW() {
+	//glewExperimental = true;										//Used to be needed for core profile?
+	if (!glfwInit()) {
+		fprintf(stderr, "Failed to init GLFW\n");					
+		exit(-1);
+	}
 
-	entt::registry reg;
-	auto view = reg.view<position, velocity>();
-	reg.destroy(view.begin(), view.end());
+	glfwWindowHint(GLFW_SAMPLES, 4);								//Anti Aliasing
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);					//Opengl version 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);			//To make macOS happy (Probably won't port)
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);	//We don't want old opengl
 
-	reg.create<position>();
-	
+	GLFWwindow *window;
+	window = glfwCreateWindow(DEFAULT_WIDTH, DEFAULT_HEIGHT, TITLE, NULL, NULL);
+
+	if (window == NULL) {
+		fprintf(stderr, "Failed to open GLFW window. Probably has to do with opengl 3.3 not being supported\n");
+		glfwTerminate();
+		exit(-1);
+	}
+
+	glfwMakeContextCurrent(window);
+	return window;
+}
+
+void initGLEW() {
+	if (glewInit() != GLEW_OK) {
+		fprintf(stderr, "Failed to init GLEW\n");
+
+		exit(-2);
+	}
+}
+
+int main(void) {
+	GLFWwindow *window = initGLFW();
+	initGLEW();
+
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+
+	do {
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	} while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) == 0);
+
+	return 0;
 }
