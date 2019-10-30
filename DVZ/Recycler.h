661811a3f1d6f745ea014fd48d7f6a4df4a1ad47
MathDotSqrt <ctrenkov@gmail.com>
2019-10-29 20:57:42 -0400

    added a mutex for recycler

--- DVZ/Recycler.h
+++ DVZ/Recycler.h
@@ -12,18 +12,22 @@ private:
 	std::vector<T*> allocations;
 	std::vector<T*> recycled;
 
+	std::mutex mutex;
+
 public:
 	Recycler(size_t alloc_size, Util::Allocator::IAllocator &parent) : pool(sizeof(T), alignof(T), alloc_size, parent){
 	
 	}
 	
 	~Recycler() {
+		std::lock_guard<std::mutex> lock(this->mutex);
 		for (T* t : allocations) {
 			Util::Allocator::free(this->pool, t);
 		}
 	}
 
 	T* getNew() {
+		std::lock_guard<std::mutex> lock(this->mutex);
 		if (!recycled.empty()) {
 			T* back = this->recycled.back();
 			this->recycled.pop_back();
@@ -35,8 +39,19 @@ public:
 		return t;
 	}
 
+	std::shared_ptr<T> getSharedNew() {
+		T *ptr = getNew();
+		return std::shared_ptr<T>(ptr, RecyclerDeletor(this));
+	}
+
+	std::unique_ptr<T> getUniqueNew() {
+		T *ptr = getNew();
+		return std::unique_ptr<T>(ptr, RecyclerDeletor(this));
+	}
+
 	template<typename ...ARGS>
 	T* getNew(ARGS ...args) {
+		std::lock_guard<std::mutex> lock(this->mutex);
 		if (!recycled.empty()) {
 			T* back = this->recycled.back();
 			this->recycled.pop_back();
@@ -50,14 +65,30 @@ public:
 
 	//todo add a handler class for this to prevent copying of pointer and funny buisness
 	void recycle(T *t) {
+		std::lock_guard<std::mutex> lock(this->mutex);
 		this->recycled.push_back(t);
 	}
 
 	size_t getNumAllocs() {
+		std::lock_guard<std::mutex> lock(this->mutex);
 		return this->allocations.size();
 	}
 
 private:
+	template<typename T>
+	struct RecyclerDeletor {
+	public:
+		RecyclerDeletor(Recycler *ptr) {
+			this->ptr = ptr;
+		}
+
+		void operator()(T *t) {
+			ptr->recycle(t);
+		}
+	private:
+		Recycler *ptr;
+	};
+
 };
 
 }
