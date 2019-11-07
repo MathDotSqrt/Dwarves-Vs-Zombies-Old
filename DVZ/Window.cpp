34675a69f456bd7387b07472cecf0f831de65744
MathDotSqrt <ctrenkov@gmail.com>
2019-11-07 16:50:27 -0500

    Disabled vsync

--- DVZ/Window.cpp
+++ DVZ/Window.cpp
@@ -40,7 +40,7 @@ void Window::createWindow(int initWidth, int initHeight, const char* initTitle) 
 	}
 
 	glfwMakeContextCurrent(window);									//Displays window and makes it active
-	glfwSwapInterval(1);											//Wheather to enable or disable vsync
+	//glfwSwapInterval(1);											//Wheather to enable or disable vsync
 
 	width = initWidth;
 	height = initHeight;
