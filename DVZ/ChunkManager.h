2c6579c5d2362b7b33551062439a518a4c1691cd
MathDotSqrt <ctrenkov@gmail.com>
2019-11-12 22:30:44 -0500

    Separated Chunk RefHandle in its own header file

--- DVZ/ChunkManager.h
+++ DVZ/ChunkManager.h
@@ -6,6 +6,8 @@
 #include "ChunkRenderData.h"
 #include "ChunkMesher.h"
 
+#include "ChunkRefHandle.h"
+
 #include "ThreadPool.h"
 
 #include "LinearAllocator.h"
@@ -20,39 +22,7 @@
 
 namespace Voxel{
 
-struct ChunkDestructor {
-public:
-	typedef std::atomic<int> RefCount;
-	ChunkDestructor(RefCount *ref) {
-		this->referenceCount = ref;
-		if (referenceCount) {
-			(*referenceCount)++;
-		}
-	}
 
-	void operator()(Chunk *) {
-		//decrements reference counter from chunk manager chunkSet
-		//when manager sees chunk having 0 references it will eventually
-		//recycle that chunk
-		if (referenceCount) {
-			(*referenceCount)--;
-		}
-	}
-private:
-	RefCount *referenceCount;
-};
-
-typedef Util::Recycler<Chunk>::UniqueHandle ChunkHandle;
-//typedef std::unique_ptr<Chunk, ChunkDestructor> ChunkRefHandle;
-
-class ChunkRefHandle : public std::unique_ptr<Chunk, ChunkDestructor> {
-public:
-	using std::unique_ptr<Chunk, ChunkDestructor>::unique_ptr;
-
-	ChunkRefHandle() : std::unique_ptr<Chunk, ChunkDestructor>(nullptr, ChunkDestructor(nullptr)){
-	
-	}
-};
 
 struct ChunkNeighbors {
 	ChunkRefHandle middle;
