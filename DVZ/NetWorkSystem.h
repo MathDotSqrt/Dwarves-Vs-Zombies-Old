c8ae6fa5ba3037ca1cb04d701cf2f51cee72c750
MathDotSqrt <ctrenkov@gmail.com>
2020-01-02 11:45:17 -0500

    Removed unneeded functionality from netcode

--- DVZ/NetWorkSystem.h
+++ DVZ/NetWorkSystem.h
@@ -1,28 +1,12 @@
 #pragma once
 #include "System.h"
-#include "entt.hpp"
-#include "Scene.h"
 
-namespace SLNet{
-	class RakPeerInterface;
-	struct Packet;
-	typedef unsigned char MessageID;
-}
 
 class NetWorkSystem : public System {
 private:
-	std::unordered_map<unsigned int, unsigned int> netToEnttID;
-
-	Graphics::Scene *scene;
-	SLNet::RakPeerInterface *peer;
-
-
-	bool isConnected = false;
-	entt::entity clientEntt = 0;
-
-	SLNet::MessageID getPacketID(SLNet::Packet *packet);
+	
 public:
-	NetWorkSystem(int priority, entt::entity clientEntt, Graphics::Scene *scene);
+	NetWorkSystem(int priority);
 	~NetWorkSystem();
 
 	void addedToEngine(Engine * engine);
