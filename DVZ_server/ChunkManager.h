3a078d6d56186360b8eda0b3710c56eb901baf41
MathDotSqrt <ctrenkov@gmail.com>
2020-05-24 16:11:06 -0400

    mow it doesnt crash with small render size??

--- DVZ_server/ChunkManager.h
+++ DVZ_server/ChunkManager.h
@@ -18,8 +18,8 @@ namespace Voxel {
 		Util::Allocator::PoolAllocator voxel_allocator;                                               
 		std::vector<Chunk> world;
 	public:
-		constexpr static int WORLD_X_LENGTH = 32;
-		constexpr static int WORLD_Z_LENGTH = 32;
+		constexpr static int WORLD_X_LENGTH = 3;
+		constexpr static int WORLD_Z_LENGTH = 3;
 		constexpr static int MIN_CHUNK_X = 1 - (WORLD_X_LENGTH + 1) / 2;
 		constexpr static int MIN_CHUNK_Z = 1 - (WORLD_Z_LENGTH + 1) / 2;
 		constexpr static int MAX_CHUNK_X = (WORLD_Z_LENGTH) / 2;
